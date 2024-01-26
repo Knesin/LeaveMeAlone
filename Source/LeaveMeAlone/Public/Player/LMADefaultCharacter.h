40a0eb4edf4078d675cf64fcaec854641ff3028a
Maksim Kl <GuruHacker@yandex.ru>
2024-01-26 13:18:49 +0300

    Initial commit

---
+++ Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
@@ -0,0 +1,59 @@
+// : LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ALMADefaultCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(20.0f, 40.0f, 40.0f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera|Zoom")
+	float ArmLengthMax = 4000.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera|Zoom")
+	float ArmLengthMin = 400.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera|Zoom")
+	float ZoomSpeed = 60.0f;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	float YRotation = -75.0f;
+	float ArmLength = 1400.0f;
+	float FOV = 55.0f;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void ZoomIn();
+	void ZoomOut();
+};
